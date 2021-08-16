72f92d0f1381310f84a996dcd1e8fa652c7690e8
Gilberto Treviño <trevigno@gmail.com>
2021-08-16 09:17:20 -0500

    Hiding secrets

--- admin/settings.py
+++ admin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from ..secrets import keys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4&jbl-(w!t(gg$_3x#@#6)06!*i(haz5dbb07fn7b9iilizn86'
+SECRET_KEY = keys.DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
